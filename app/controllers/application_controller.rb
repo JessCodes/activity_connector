f644bf813e5f180724670304abcbe380ddb7935e
JessCodes <jessicafellison@gmail.com>
2016-12-22 16:38:18 -0800

    clean up comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  # def weather
-  #   Http::get.configure(API_KEY)
-  #   weather_data = Http::get({url: "weather.com/today?lat=5656&long=fgfdghdfhg"})
-  #   # handle timeout of api
-  #   # retry
-  #   # handle other errors and return something anyway
-  #   render :partial => 'weather'
-  # end
-
 end
