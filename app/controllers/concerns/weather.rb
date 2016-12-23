43b3d75ae439c15e06d2b14b00bcfe292016f466
JessCodes <jessicafellison@gmail.com>
2016-12-22 16:52:04 -0800

    display weather info

--- app/controllers/concerns/weather.rb
+++ app/controllers/concerns/weather.rb
@@ -10,13 +10,12 @@ class Weather
 	end 
 
 	def sf_forecast
-		@response = self.class.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=sanfrancisco&mode=json&units=imperial&cnt=7&APPID=#{ENV['OWM_key']}")
+		p @response = self.class.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=sanfrancisco&mode=json&units=imperial&cnt=7&APPID=#{ENV['OWM_key']}")
 		parse_weather_json
 	end 
 
 	def parse_weather_json
 		p @temp = @response['list'][0]['temp']['day']
 		p @description = @response['list'][0]['weather'][0]['description']
-		p @icon = @response['list'][0]['weather'][0]['icon']
 	end
 end 
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,8 +1,5 @@
 class IndexController < ApplicationController
   def index
   	@weather = Weather.new
-  	@weather.temp
-  	@weather.description
-  	@weather.icon
   end
 end
