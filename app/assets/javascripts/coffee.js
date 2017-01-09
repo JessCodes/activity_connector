2f27206728054a06c45d3beb0e21d6187596c3b4
JessCodes <jessicafellison@gmail.com>
2017-01-08 19:13:56 -0800

    add zoomable map with one marker for cafe

--- app/assets/javascripts/coffee.js
+++ app/assets/javascripts/coffee.js
@@ -1,7 +1,7 @@
 function initMap() {
   var cs1 = {lat: 37.776175, lng: -122.423220};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 14,
     center: cs1
   });
   var marker = new google.maps.Marker({
@@ -9,3 +9,13 @@ function initMap() {
     map: map
   });
 }
+
+  // var cs2 = {lat: 37.798641, lng: -122.407344};
+  // var cs3 = {lat: 37.763301, lng: -122.410755};
+  // var cs4 = {lat: 37.767019, lng: -122.421781};
+  // var cs5 = {lat: 37.752345, lng: -122.414309};
+  // var cs6 = {lat: 37.756439, lng: -122.421383};
+  // var cs7 = {lat: 37.776900, lng: -122.408637};
+  // var cs8 = {lat: 37.763162, lng: -122.465974};
+  // var cs9 = {lat: 37.796751, lng: -122.436629};
+  // var cs10 = {lat: 37.790770, lng: -122.402015};
