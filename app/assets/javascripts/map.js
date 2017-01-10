97a2a3ea2ea3188c88eaaeb12e58a645648b5ece
meganeding1git config --global user.email eding.megan@gmail.comgit config --global core.editor subl <eding.megan@gmail.com>
2017-01-09 17:54:10 -0800

    create movie map with markers

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -110,4 +110,61 @@ function initMap() {
     map: hikeMap
   });
 
+	var movie1 = {lat: 37.762033, lng: -122.434759};
+  var movie2 = {lat: 37.794997, lng: -122.399435};
+  var movie3 = {lat: 37.784983, lng: -122.432637};
+  var movie4 = {lat: 37.764710, lng: -122.422408};
+  var movie5 = {lat: 37.782366, lng: -122.482909};
+  var movie6 = {lat: 37.756216, lng: -122.419191};
+  var movie7 = {lat: 37.764859, lng: -122.418865};
+  var movie8 = {lat: 37.784272, lng: -122.403159};
+  var movie9 = {lat: 37.790472, lng: -122.434533};
+  var movie10 = {lat: 37.786828, lng: -122.410628};
+
+  var movieMap = new google.maps.Map(document.getElementById('movie-map'), {
+    zoom: 13,
+    center: movie3
+  });
+  var movieMarker1 = new google.maps.Marker({
+    position: movie1,
+    map: movieMap
+  });
+  var movieMarker2 = new google.maps.Marker({
+    position: movie2,
+    map: movieMap
+  });
+  var movieMarker3 = new google.maps.Marker({
+    position: movie3,
+    map: movieMap
+  });
+  var movieMarker4 = new google.maps.Marker({
+    position: movie4,
+    map: movieMap
+  });
+  var movieMarker5 = new google.maps.Marker({
+    position: movie5,
+    map: movieMap
+  });
+  var movieMarker6 = new google.maps.Marker({
+    position: movie6,
+    map: movieMap
+  });
+  var movieMarker7 = new google.maps.Marker({
+    position: movie7,
+    map: movieMap
+  });
+  var movieMarker8 = new google.maps.Marker({
+    position: movie8,
+    map: movieMap
+  });
+  var movieMarker9 = new google.maps.Marker({
+    position: movie9,
+    map: movieMap
+  });
+  var movieMarker10 = new google.maps.Marker({
+    position: movie10,
+    map: movieMap
+  });
+
+
 }
